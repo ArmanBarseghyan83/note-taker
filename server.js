30bbdf8977fdc48dc2e91395ed5f9061b1124ec4
Arman <armanbarseghyan83@gmail.com>
2023-10-29 10:55:44 -0700

    GET /api/notes and POST /api/notes routes created

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 const express = require('express')
+const uniqid = require('uniqid')
 const path = require('path')
+
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const app = express()
 const PORT = 3001
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'))
+
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+})
+
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uniqid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.json('Error in adding note');
+    }
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 
